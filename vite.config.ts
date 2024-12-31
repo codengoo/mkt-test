da9fb9439b4461659e72d0abb3bb08b2e7226f3d
NghiaDT <nghiadt22.work@gmail.com>
2024-12-31 12:19:41 +0700

    abc

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import packageJson from './package.json'
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      external: [
+        ...Object.keys(packageJson.dependencies || {}),
+        ...Object.keys(packageJson.devDependencies || {}),
+        /^node:/,
+        /^vitechgroup:/,
+      ],
+    },
+
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       formats: ["es", "cjs"],
-      name: "test",
+      name: "index",
       fileName: "index",
     },
   },
